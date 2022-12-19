fa0130c826c7f122368ff35ae72d0658c9c7c7c8
Dydek123 <mazurkiewicz707@gmail.com>
2022-12-19 20:22:23 +0100

    Create simple structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ProfileComponent} from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainPageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
