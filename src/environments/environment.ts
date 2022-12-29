76be3437a733166a1e4cef63dd03a54344f869ad
Dydek123 <mazurkiewicz707@gmail.com>
2022-12-29 21:29:48 +0100

    Create firebase authentication using google providers

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "http://localhost:3000",
+  firebaseConfig: {
+    apiKey: "AIzaSyBohjDBZEWsEUCJ4Y8u-YM8D__AJThtLmo",
+    authDomain: "blablacar-d3d3f.firebaseapp.com",
+    projectId: "blablacar-d3d3f",
+    storageBucket: "blablacar-d3d3f.appspot.com",
+    messagingSenderId: "33637923186",
+    appId: "1:33637923186:web:a89fddcd29af42bf3205eb",
+    measurementId: "G-7W6QP99JBF"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
