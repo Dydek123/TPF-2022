5ce2381a99df002b1f7fb43a17e70b8f91092886
Dydek123 <mazurkiewicz707@gmail.com>
2023-01-31 18:57:03 +0100

    Get user travels

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import {Context} from "../../shared/context";
 import {CardStatus} from "../../shared/card-status";
 import {Travel} from "../../shared/models/travel.model";
 import {TravellService} from "../../services/travell.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-profile',
@@ -15,17 +16,27 @@ export class ProfileComponent implements OnInit {
   travels: Travel[];
   selectedTravel: Travel | null;
 
-  constructor(private travelService: TravellService) {
+  constructor(private travelService: TravellService,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
-    this.travelService.getAll().subscribe(travels => {
-      this.travels = travels;
-      this.selectedTravel = travels.length > 0 ? travels[0] : null;
-    });
+    this.authService.getUser().subscribe(user => {
+      if (user?.uid) {
+        this.getTravels(user.uid);
+      }
+    })
   }
 
   onCardChanged(event: Travel) {
     this.selectedTravel = event;
+  }
+
+  private getTravels(uid: string) {
+    this.travelService.getByUserId(uid).subscribe(travels => {
+      console.log(travels);
+      this.travels = travels;
+      this.selectedTravel = travels.length > 0 ? travels[0] : null;
+    });
   }
 }
